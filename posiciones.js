aecf5d1c3dc1be1421b4338a694bf054a1539d69
Ignacio de Osma <Ignacio@dhcp-10-105-103-163.wireless.northwestern.private>
2018-05-12 18:14:40 -0500

    .

--- posiciones.js
+++ posiciones.js
@@ -1,4 +1,3 @@
-// Initialize Firebase
 var config = {
   apiKey: "AIzaSyDo5wHOoM32PLAe5Psf5gPOZOAI5G5bKvU",
   authDomain: "polla-45628.firebaseapp.com",
@@ -9,19 +8,63 @@ var config = {
 };
 firebase.initializeApp(config);
 
-var Nusers;
+var database = firebase.database();
+var ref = database.ref("Local");
+var ref_names = database.ref("Names");
+ref.on('value', gotData, errData);
 
-function init(){
-  var table = document.getElementById("positions");
-  for(int i = 0; i < Nusers; i++){
-    var row = table.insertRow(1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    cell1.innerHTML = i;
-    cell2.innerHTML = "name";
-    cell3.innerHTML = 0;
+var Nusers = 0;
+var names = [];
+var pts = [];
+
+function gotData(data){
+  var nusers = data.val();
+  var keys = Object.keys(nusers);
+  Nusers = parseInt(nusers[keys[0]]);
+  var nms = data.val();
+  var keys = Object.keys(nms);
+  // init();
+}
+
+console.log(database.ref().child('object'));
+
+function getNames(data){
+  var nms = data.val();
+  var keys = Object.keys(nms);
+  for(var i = 0; i < keys.length; i++){
+    var k = keys[i];
+    var val = nms[k].name;
+    names.push(val);
+    console.log(i);
+    console.log(val, "end loop");
   }
 }
 
-init();
+function getPoints(data){
+  for(i = 0; i < Nusers; i++){
+    name.on('value', function(dta){
+      var name = database.ref(val);
+      var x = dta.val();
+      var kys = Object.keys(x);
+      point = x[kys[56]];
+      pts.push(point);
+    });
+  }
+  init(i);
+}
+
+function errData(err){
+  console.log("Error");
+  console.log(err);
+}
+
+function init(index){
+  var table = document.getElementById("positions");
+  var row = table.insertRow(1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  cell1.innerHTML = index;
+  cell2.innerHTML = names[i];
+  cell3.innerHTML = pts[i];
+}
