e16dd617ca703a20ee84db9607ffaf0828eec538
Ignacio de Osma <Ignacio@Ignacios-MBP.attlocal.net>
2018-05-21 18:36:46 -0500

    .

--- predicciones.js
+++ predicciones.js
@@ -608,3 +608,16 @@ autocomplete(document.getElementById("arquero"), arqueros);
 autocomplete(document.getElementById("jugador_joven"), jugadores_jovenes);
 autocomplete(document.getElementById("mejor_jugador"), jugadores);
 autocomplete(document.getElementById("goleador"), jugadores);
+
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
+
+deleteAllCookies();
