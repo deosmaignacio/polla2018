4a0cf1a1564b329df42a11f7e336349e3ea5da2a
Juan Diego Vidaurrazaga <jdvidaurrazaga@MBP-JD.local>
2018-06-05 20:05:22 +0200

    pickers y clasificados del simulador

---
+++ pickers.js
@@ -0,0 +1,19 @@
+var config = {
+  apiKey: "AIzaSyDo5wHOoM32PLAe5Psf5gPOZOAI5G5bKvU",
+  authDomain: "polla-45628.firebaseapp.com",
+  databaseURL: "https://polla-45628.firebaseio.com",
+  projectId: "polla-45628",
+  storageBucket: "",
+  messagingSenderId: "198637023867"
+};
+firebase.initializeApp(config);
+
+function User(name, points){
+  this.name = name;
+  this.points = points;
+}
+
+function add_user(name, points){
+  var user = new User(name, points);
+  users.push(user);
+}
