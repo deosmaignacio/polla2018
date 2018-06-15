a2486908b06dda5574ce4c4e59491572dd0140b1
Ignacio de Osma <Ignacio@Ignacios-MacBook-Pro.local>
2018-06-14 20:29:00 -0400

    .

--- posiciones.js
+++ posiciones.js
@@ -12,7 +12,6 @@ function User(name, points){
   this.name = name;
   this.points = points;
 }
-console.log("enter");
 function add_user(name, points){
   var user = new User(name, points);
   users.push(user);
@@ -41,82 +40,78 @@ function getMatch(homeTeam, awayTeam){
   }
 }
 
+var database = firebase.database();
+
 var users = [];
 var matches = [];
 groups = ["A", "B", "C", "D", "E", "F", "G", "H"];
 
 var Nusers = 0;
-var Ngames = games();
+var Ngames;
 
-for(var i = 1; i < 49; i++){
-  var currHomeTeam = document.getElementById("T"+i+"H").innerHTML;
-  var currAwayTeam = document.getElementById("T"+i+"A").innerHTML;
-  var currHomeScore = document.getElementById("R"+i+"H").innerHTML;
-  var currAwayScore = document.getElementById("R"+i+"A").innerHTML;
-  if(currHomeScore.length == 0){
-    currHomeScore = "vacio";
-  }
-  if(currAwayScore.length == 0){
-    currAwayScore = "vacio";
-  }
-  var currMatch = getMatch(currHomeTeam, currAwayTeam);
-  var dbHomeScore = currMatch.homeScore;
-  var dbAwayScore = currMatch.awayScore;
-  if((dbHomeScore != currHomeScore) && (dbAwayScore != currAwayScore)){
-    document.getElementById("R"+i+"H").innerHTML = dbHomeScore;
-    document.getElementById("R"+i+"A").innerHTML = dbAwayScore;
-  }
-}
+dbGames();
 
-var database = firebase.database();
-var ref = database.ref().once('value', function(snap){
-  Nusers = snap.numChildren() - 2;
-  snap.forEach(userSnap => {
-    var name = userSnap.key;
-    if(name == "Scores"){
-      var ref_match = database.ref().child("Scores").once('value', function(snapMatch){
-        Ngames_db = snapMatch.val().Ngames;
-        points_db = snapMatch.val().points;
-        snapMatch.forEach(matchSnap =>{
-          var match = matchSnap.val();
-          if(match != Ngames_db && match != points_db){
-            var homeTeam = match.home;
-            var awayTeam = match.away;
-            var homeScore = match.home_score;
-            var awayScore = match.away_score;
-            addMatch(homeTeam, awayTeam, homeScore, awayScore);
-          }
-        })
-      });
-    }
-    else if (name != "Codes"){
-      var points_user = 0;
-      var ref_name = database.ref().child(name).once('value', data =>{
-        var games = database.ref().child(name).child("Games").once('value', function(snapGames){
-          snapGames.forEach(GameSnap =>{
-            var game = GameSnap.val();
-            var home = game.home;
-            var away = game.away;
-            var home_score = game.home_score;
-            var away_score = game.away_score;
-            for(var i = 1; i < Ngames + 1; i++){
-              var home_team = document.getElementById("T"+i+"H").innerHTML;
-              var home_goals = document.getElementById("R"+i+"H").innerHTML;
-              var away_team = document.getElementById("T"+i+"A").innerHTML;
-              var away_goals = document.getElementById("R"+i+"A").innerHTML;
-              if((home_team == home) && (away_team == away)){
-                points_user = calculate_pts(home_score, away_score, home_goals, away_goals) + points_user;
-              }
+var stopper = false;
+
+function general(NumberOfGames){
+  Ngames = NumberOfGames;
+  var ref = database.ref().once('value', function(snap){
+    Nusers = snap.numChildren() - 2;
+    snap.forEach(userSnap => {
+      var name = userSnap.key;
+      if(name == "Scores"){
+        var ref_match = database.ref().child("Scores").once('value', function(snapMatch){
+          Ngames_db = snapMatch.val().Ngames;
+          points_db = snapMatch.val().points;
+          snapMatch.forEach(matchSnap =>{
+            var match = matchSnap.val();
+            if(match != Ngames_db && match != points_db){
+              var homeTeam = match.home;
+              var awayTeam = match.away;
+              var homeScore = match.home_score;
+              var awayScore = match.away_score;
+              addMatch(homeTeam, awayTeam, homeScore, awayScore);
             }
           })
-          database.ref().child(name).update({points: points_user});
-          add_user(name, points_user);
-          init();
+          if(matches.length == 48){
+            init_scores();
+          }
         });
-      });
-  }
-  })
-});
+      }
+      else if (name != "Codes"){
+        var points_user = 0;
+        var ref_name = database.ref().child(name).once('value', data =>{
+          var games = database.ref().child(name).child("Games").once('value', function(snapGames){
+            snapGames.forEach(GameSnap =>{
+              var game = GameSnap.val();
+              var home = game.home;
+              var away = game.away;
+              var home_score = game.home_score;
+              var away_score = game.away_score;
+              console.log(Ngames);
+              for(var i = 1; i < Ngames + 1; i++){
+                console.log("enter");
+                if(matches.length != 48){
+                  console.log("not 48");
+                }
+                var home_team = document.getElementById("T"+i+"H").innerHTML;
+                var home_goals = document.getElementById("R"+i+"H").innerHTML;
+                var away_team = document.getElementById("T"+i+"A").innerHTML;
+                var away_goals = document.getElementById("R"+i+"A").innerHTML;
+                if((home_team == home) && (away_team == away)){
+                  points_user = calculate_pts(home_score, away_score, home_goals, away_goals) + points_user;
+                }
+              }
+            })
+            database.ref().child(name).update({points: points_user});
+            add_user(name, points_user);
+            init();
+          });
+        });
+    }
+    })
+  });
+}
 
 function games(){
   var result = 0;
@@ -131,6 +126,13 @@ function games(){
     }
   }
 }
+
+function dbGames(){
+  var ref_match = database.ref().child("Scores").once('value', function(snap){
+    general(snap.val().Ngames);
+  });
+}
+
 
 function calculate_pts(home_guess, away_guess, home_score, away_score){
   var dg_real = home_score - away_score;
@@ -154,26 +156,69 @@ function calculate_pts(home_guess, away_guess, home_score, away_score){
 }
 
 function init_scores(){
-  for(var i = 1; i < 49; i++){
-    var currHomeTeam = document.getElementById("T"+i+"H").innerHTML;
-    var currAwayTeam = document.getElementById("T"+i+"A").innerHTML;
-    var currHomeScore = document.getElementById("R"+i+"H").innerHTML;
-    var currAwayScore = document.getElementById("R"+i+"A").innerHTML;
-    if(currHomeScore.length == 0){
-      currHomeScore = "vacio";
-    }
-    if(currAwayScore.length == 0){
-      currAwayScore = "vacio";
-    }
-    var currMatch = getMatch(currHomeTeam, currAwayTeam);
-    var dbHomeScore = currMatch.homeScore;
-    var dbAwayScore = currMatch.awayScore;
-    if((dbHomeScore != currHomeScore) && (dbAwayScore != currAwayScore)){
-      document.getElementById("R"+i+"H").innerHTML = dbHomeScore;
-      document.getElementById("R"+i+"A").innerHTML = dbAwayScore;
+  if(stopper){
+    for(var i = 1; i < 49; i++){
+        var currHomeTeam = document.getElementById("T"+i+"H").innerHTML;
+        var currAwayTeam = document.getElementById("T"+i+"A").innerHTML;
+        var currHomeScore = document.getElementById("R"+i+"H").innerHTML;
+        var currAwayScore = document.getElementById("R"+i+"A").innerHTML;
+        if(currHomeScore.length == 0){
+          currHomeScore = "vacio";
+        }
+        if(currAwayScore.length == 0){
+          currAwayScore = "vacio";
+        }
+        var currMatch = getMatch(currHomeTeam, currAwayTeam);
+        var dbHomeScore = currMatch.homeScore;
+        var dbAwayScore = currMatch.awayScore;
+        if((dbHomeScore != currHomeScore) && (dbAwayScore != currAwayScore)){
+          document.getElementById("R"+i+"H").innerHTML = dbHomeScore;
+          document.getElementById("R"+i+"A").innerHTML = dbAwayScore;
+        }
     }
   }
+}
 
+function init(){
+  if(Nusers == users.length){
+    document.getElementById("espere").innerHTML=""
+    users.sort(compare);
+    var table = document.getElementById("positions");
+    for(var i = 0; i < users.length; i++){
+      var row = table.insertRow(1);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      var cell3 = row.insertCell(2);
+      cell1.innerHTML = users.length-i;
+      cell2.innerHTML = users[users.length-i-1].name;
+      cell2.setAttribute("class", "link_users");
+      var place = users.length-i
+      cell2.setAttribute("onclick","user_predictions("+place+")");
+      cell3.innerHTML = users[users.length-i-1].points;
+    }
+  }
+}
+
+function init_scores(){
+  for(var i = 1; i < 49; i++){
+      var currHomeTeam = document.getElementById("T"+i+"H").innerHTML;
+      var currAwayTeam = document.getElementById("T"+i+"A").innerHTML;
+      var currHomeScore = document.getElementById("R"+i+"H").innerHTML;
+      var currAwayScore = document.getElementById("R"+i+"A").innerHTML;
+      if(currHomeScore.length == 0){
+        currHomeScore = "vacio";
+      }
+      if(currAwayScore.length == 0){
+        currAwayScore = "vacio";
+      }
+      var currMatch = getMatch(currHomeTeam, currAwayTeam);
+      var dbHomeScore = currMatch.homeScore;
+      var dbAwayScore = currMatch.awayScore;
+      if((dbHomeScore != currHomeScore) && (dbAwayScore != currAwayScore)){
+        document.getElementById("R"+i+"H").innerHTML = dbHomeScore;
+        document.getElementById("R"+i+"A").innerHTML = dbAwayScore;
+      }
+  }
 }
 
 function user_predictions(place){
